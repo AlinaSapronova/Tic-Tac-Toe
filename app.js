0eb7cb966d131aefbc289d41e81f5020b3df579b
alina <sapron4ik93@gmail.com>
2022-06-22 14:32:15 -0500

    think about two players

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const area = document.querySelector('.tictactoe');
+const btn = document.querySelector('.btn');
+const playBtn = document.querySelector('.playBtn');
 let start = 0;
-let result = "";
+
+playBtn.addEventListener('click', choose);
+function choose(e) {
+    e.preventDefault();
+    
+
+}
+
+
+
+
+
 
 area.addEventListener('click', startMove)
 function startMove(e) {
     if(e.target.className = 'block') {
         //console.log(e.target)
-        start % 2 === 0 ? e.target.innerHTML = 'X' : e.target. innerHTML = '0';
-        start ++
-         checking()
+        start % 2 === 0 ? e.target.innerHTML = 'X' : e.target.innerHTML = '0';
+        start ++;
+        checking()
+         
     }
 }
 
@@ -28,16 +42,46 @@ function checking() {
     ];
     for(let i = 0; i < arrays.length; i++) {
         if(blocks[arrays[i][0]].innerHTML == 'X' && blocks[arrays[i][1]].innerHTML == 'X' && blocks[arrays[i][2]].innerHTML == 'X') {
-            result = 'X';
-            winner(result)
+           return alert('You WINER!!!')
         } 
         else if(blocks[arrays[i][0]].innerHTML == '0' && blocks[arrays[i][1]].innerHTML == '0' && blocks[arrays[i][2]].innerHTML == '0') {
-            result = '0';
-            winner(result)
+           return alert('You WINER!!!')
+        }
+        else if(start > 7) {
+          return  alert('DRAW');
         }
     }
 }
 
-function winner() {
-    console.log(winner)
+
+
+
+//function with random number (0,8)
+
+function randomName() {
+let number = Math.floor(Math.random() * 8)
+return number
+//console.log(number)
 }
+randomName()
+
+function currentEmpty() {
+    
+}
+
+
+
+// need another f to check if the cell current empty
+//if it empty then computer have to mark this
+//if it not emty the run random number again
+
+
+
+
+
+
+
+
+btn.addEventListener('click', () => {
+    location.reload();
+})
